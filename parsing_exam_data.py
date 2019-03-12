13318dd9713b429ebcac525f34c9aa89043454e4
JiKook31 <a3198i@rambler.ru>
2019-03-12 23:45:27 +0300

    csv representation for universities and code fix

--- parsing_exam_data.py
+++ parsing_exam_data.py
@@ -11,31 +11,31 @@ subjects_translate = {'obzestvoznanie': "Обществознание", 'russkiy
                       'fizika': "Физика", 'literatura': "Литература",
                       'history': "История", 'matematika': "Математика", 'lang':"Английский язык"}
 end_url = 'Submit.x=60&Submit.y=23'
+#
+# spec_id_dict = {}
+# id_spec_dict = {}
+#
+# specs_url = 'https://moeobrazovanie.ru/specialities_vuz/'
+# r = requests.get(specs_url)
+# text = r.text
+# soup = BeautifulSoup(text, 'html.parser')
+# for a in soup.find_all("a"):
+#     if a.get('class') == ['spec_articles_list_spec_link']:
+#         link = a.get('href').split('/')[-1]
+#         new_url = specs_url + link
+#         r = requests.get(new_url)
+#         text = r.text
+#         soup_new = BeautifulSoup(text, 'html.parser')
+#         name = soup_new.find_all("h1")[1].get_text()
+#         for td in soup_new.find_all("td"):
+#             if td.get_text().startswith("Код") \
+#                     and not td.get('class') == ['specHelmImg']:
+#                 id = str(list(td.children)[0]).split(" ")[-1]
+#                 break
+#         spec_id_dict[name] = [id]
+#         # id_spec_dict[id] = name
 
-spec_id_dict = {}
-id_spec_dict = {}
-
-specs_url = 'https://moeobrazovanie.ru/specialities_vuz/'
-r = requests.get(specs_url)
-text = r.text
-soup = BeautifulSoup(text, 'html.parser')
-for a in soup.find_all("a"):
-    if a.get('class') == ['spec_articles_list_spec_link']:
-        link = a.get('href').split('/')[-1]
-        new_url = specs_url + link
-        r = requests.get(new_url)
-        text = r.text
-        soup_new = BeautifulSoup(text, 'html.parser')
-        name = soup_new.find_all("h1")[1].get_text()
-        for td in soup_new.find_all("td"):
-            if td.get_text().startswith("Код") \
-                    and not td.get('class') == ['specHelmImg']:
-                id = str(list(td.children)[0]).split(" ")[-1]
-                break
-        spec_id_dict[name] = [id]
-        id_spec_dict[id] = name
-
-js = open('speciality_id.json', encoding='utf-8').read()
+js = open('id_speciality.json', encoding='utf-8').read()
 id_spec_dict = json.loads(js)
 
 middle_url = ""
@@ -61,7 +61,7 @@ for td in soup.find_all('td'):
     if td.get('style') == 'white-space:nowrap;':
         list_grades.append(td.get_text())
     if td.get('class') == ['light_gray_back', 'gray_border_right']:
-        list_ids.append(td.get_text())
+        list_ids.append(td.get_text().strip())
     if td.find('div') != None and \
             td.find('div').get("class") == ['small_text', 'gray_text']:
         subjects = td.find('div').get_text()
@@ -71,54 +71,65 @@ for td in soup.find_all('td'):
 for i in range(len(list_universities)):
     try:
         if list_universities[i] in universities:
-            universities[list_universities[i]].append((id_spec_dict[list_ids[i]], list_ids[i],
-                                                       list_grades[i], list_subjects[i],
+            universities[list_universities[i]].append((list_ids[i], id_spec_dict[list_ids[i]],
+                                                       list_subjects[i], list_grades[i],
                                                        count_subjects[i]))
         else:
-            universities[list_universities[i]] = [(id_spec_dict[list_ids[i]], list_ids[i],
-                                                   list_grades[i], list_subjects[i],
+            universities[list_universities[i]] = [(list_ids[i], id_spec_dict[list_ids[i]],
+                                                   list_subjects[i], list_grades[i],
                                                    count_subjects[i])]
     except KeyError:
         if list_universities[i] in universities:
-            universities[list_universities[i]].append(("", list_ids[i],
-                                                       list_grades[i], list_subjects[i],
+            universities[list_universities[i]].append((list_ids[i], "",
+                                                       list_subjects[i], list_grades[i],
                                                        count_subjects[i]))
         else:
-            universities[list_universities[i]] = [("", list_ids[i],
-                                                   list_grades[i], list_subjects[i],
+            universities[list_universities[i]] = [(list_ids[i], "",
+                                                   list_subjects[i], list_grades[i],
                                                    count_subjects[i])]
 
+with open('universities.csv', 'w', encoding='utf-8') as f:
+    f.write("University, Spec_ID, Spec_Name, Subjects, Threshold, Subjects_Count\n")
+    for key in universities:
+        for i in range(len(universities[key])):
+            result = '"%s",' %key
+            for item in universities[key][i]:
+                result += '"%s",' %item
+            result = result[:-1] + "\n"
+            f.write(result)
 
-with open('universities.json', 'w', encoding="utf-8") as fp:
-    json.dump(universities, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
-
-new_url = 'https://www.ucheba.ru/for-abiturients/speciality'
-r = requests.get(new_url)
-text = r.text
-soup = BeautifulSoup(text, 'html.parser')
-for div in soup.find_all("div"):
-    if div.get('class') == ['col-sm-4', 'fs-large', 'mb-15']:
-        speciality_id = str(div.find('a').get('href')).split('/')[-1]
-        speciality_name = div.get_text().replace("\n", "")
-
-        url_speciality = new_url + "/" + str(speciality_id)
-        r = requests.get(url_speciality)
-        text = r.text
-        soup_speciality = BeautifulSoup(text, 'html.parser')
-        for div_spec in soup_speciality.find_all("div"):
-            if div_spec.get('class') == ['mb-section']:
-                for h2 in div_spec.find_all('h2'):
-                    if h2.get_text() == 'Кем работать':
-                        spec_descr = div_spec.get_text().replace("\n", "").\
-                            replace("Кем работать", "").replace("\t", "")
-                    if h2.get_text() == 'Перспективы':
-                        spec_perspecs = div_spec.get_text().replace("\n", "").\
-                            replace("Перспективы", "").replace("\t", "")
-
-        try:
-            spec_id_dict[speciality_name].append((spec_descr, spec_perspecs))
-        except KeyError:
-            continue
-
-with open('speciality.json', 'w', encoding="utf-8") as fp:
-    json.dump(spec_id_dict, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
+# new_url = 'https://www.ucheba.ru/for-abiturients/speciality'
+# r = requests.get(new_url)
+# text = r.text
+# soup = BeautifulSoup(text, 'html.parser')
+# for div in soup.find_all("div"):
+#     if div.get('class') == ['col-sm-4', 'fs-large', 'mb-15']:
+#         speciality_id = str(div.find('a').get('href')).split('/')[-1]
+#         speciality_name = div.get_text().replace("\n", "")
+#
+#         url_speciality = new_url + "/" + str(speciality_id)
+#         r = requests.get(url_speciality)
+#         text = r.text
+#         soup_speciality = BeautifulSoup(text, 'html.parser')
+#         for div_spec in soup_speciality.find_all("div"):
+#             if div_spec.get('class') == ['mb-section']:
+#                 for h2 in div_spec.find_all('h2'):
+#                     if h2.get_text() == 'Кем работать':
+#                         spec_descr = div_spec.get_text().replace("\n", "").\
+#                             replace("Кем работать", "").replace("\t", "")
+#                     if h2.get_text() == 'Перспективы':
+#                         spec_perspecs = div_spec.get_text().replace("\n", "").\
+#                             replace("Перспективы", "").replace("\t", "")
+#
+#         try:
+#             spec_id_dict[speciality_name].append(spec_descr)
+#             spec_id_dict[speciality_name].append(spec_perspecs)
+#         except KeyError:
+#             continue
+#
+# with open('specialities.csv', 'w', encoding='utf-8') as f:
+#     for key in spec_id_dict.keys():
+#         try:
+#             f.write('"%s",%s,"%s","%s"\n' %(key, spec_id_dict[key][0], spec_id_dict[key][1], spec_id_dict[key][2]))
+#         except IndexError:
+#             f.write('"%s",%s,,\n' %(key, spec_id_dict[key][0]))
